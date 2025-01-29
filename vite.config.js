5f3754565fa12fcb5269581aa2ed031fa070a7c8
Ava Williams <ava.williamsx2@gmail.com>
2025-01-29 00:08:43 -0800

    Configured base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/confif/
 export default defineConfig({
-    base: '/My-First-Vue-App-Deploy/',
-    plugins: [vue()]
+  plugins: [vue()],
+  base: '/My-First-Vue-App/' 
 })
